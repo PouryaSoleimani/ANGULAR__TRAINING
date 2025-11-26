512c5d4e4e177b7c4591c18aa747f2758b0b5c3a
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-26 23:08:03 +0330

    ANGULAR__TRAINING ==> COMPONENTS

--- src/app/Footer/footer.ts
+++ src/app/Footer/footer.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'FooterComponent',
   imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
