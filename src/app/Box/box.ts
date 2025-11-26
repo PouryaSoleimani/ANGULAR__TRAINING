bb4a96bddc0e9712eddf2ca04e716e2e314ddfe9
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-26 22:39:05 +0330

    COMPONENTS

--- src/app/Box/box.ts
+++ src/app/Box/box.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-box',
+  selector: 'AppBox',
   imports: [],
   templateUrl: './box.html',
   styleUrl: './box.css',
--- src/app/Header/Header.ts
+++ src/app/Header/Header.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'HeaderComponent',
   imports: [],
   templateUrl: './Header.html',
   styleUrl: './Header.css',
