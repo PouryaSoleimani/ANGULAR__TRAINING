4a68a956e604c0fd49dc4d73c4f691e78a6ef6b5
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-11-26 22:04:42 +0330

    COMPONENTS

---
+++ src/app/Header/Header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '',
+  imports: [],
+  templateUrl: './Header.html',
+  styleUrl: './Header.css',
+})
+export class Header {
+  public title = 'HEADER';
+}
